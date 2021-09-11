fbca825c31d3ff0859c6e94cdd6a513317c0e670
vladislav-slobodenjuk <vladjun@gmail.com>
2021-09-11 16:00:31 +0300

    gallery markup added

--- app.js
+++ app.js
@@ -63,3 +63,48 @@ const galleryItems = [
     description: 'Lighthouse Coast Sea',
   },
 ];
+
+const refs = {
+  gallery: document.querySelector('.js-gallery'),
+  lightbox: document.querySelector('.js-lightbox'),
+};
+
+const { gallery, lightbox } = refs;
+
+const ItemMarkup = `<li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+  >
+    <img
+      class="gallery__image"
+      src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
+      data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+      alt="Tulips"
+    />
+  </a>
+</li>`;
+
+function createItems(arr) {
+  return arr
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a
+    class="gallery__link"
+    href=${original}
+  >
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</li>`;
+    })
+    .join('');
+}
+
+const markup = createItems(galleryItems);
+
+gallery.insertAdjacentHTML('afterbegin', markup);
